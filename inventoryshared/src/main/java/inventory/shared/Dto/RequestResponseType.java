5959c79e665704db9d936bfa38e9789513cf0819
cherny77 <kyrylo.chornokozynskyi@ukma.edu.ua>
2020-06-25 22:08:56 +0300

    fixed RequestResponseType and added some addictions to request and response classes

--- inventoryshared/src/main/java/inventory/shared/Dto/RequestResponseType.java
+++ inventoryshared/src/main/java/inventory/shared/Dto/RequestResponseType.java
@@ -12,11 +12,11 @@ public enum RequestResponseType {
 	FIND_GOODS(String.class, GoodsDto[].class),
 	GET_ALL_GOODS(null, GoodsDto[].class),
 	GET_ALL_GROUPS(null,GroupDto[].class),
-	AUTH(UserDto.class, null),
-	ADD_GROUP(GroupDto.class, null),
-	ADD_GOODS(GoodsDto.class, null),
-	REMOVE_GROUP(GroupDto.class, null),
-	REMOVE_GOODS(GoodsDto.class, null),
+	AUTH(UserDto.class, null), // to do
+	ADD_GROUP(GroupDto.class, GoodsDto[].class),
+	ADD_GOODS(GoodsDto.class, GoodsDto[].class),
+	REMOVE_GROUP(GroupDto.class, GroupDto[].class),
+	REMOVE_GOODS(GoodsDto.class, GoodsDto[].class),
 	CHANGE_GOODS_QUANTITY(ChangeGoodsQuantityDto.class, null);
 
 	private Class<?> requestKlass;
