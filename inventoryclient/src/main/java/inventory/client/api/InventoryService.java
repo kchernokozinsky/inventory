f839f6eeb4aeb6b5a67e9693bafd80b91b5c8016
cherny77 <kyrylo.chornokozynskyi@ukma.edu.ua>
2020-06-25 09:42:25 +0300

    fix pom

--- inventoryclient/src/main/java/inventory/client/api/InventoryService.java
+++ inventoryclient/src/main/java/inventory/client/api/InventoryService.java
@@ -1,7 +1,19 @@
 package inventory.client.api;
 
+
+
+
+
+
+
+
+import DTO.GroupDTO;
+
+import java.util.ArrayList;
+
 public interface InventoryService {
 
-
+private GroupDTO;
+//	ArrayList<GroupDTO>
 
 }
--- inventoryorm/src/main/java/services/Main.java
+++ inventoryorm/src/main/java/services/Main.java
@@ -1,12 +1,13 @@
 package services;
 
+import DTO.GroupDTO;
+
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-
 
     }
 }
